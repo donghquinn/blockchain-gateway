63df2a66d7d50e440a86d14b27527bbe393549ef
dong hyun kim <ehdgus1524@gmail.com>
2024-12-12 17:54:13 +0900

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module org.donghyusn.com/chain/collector
+
+go 1.23.4
---
+++ utils/fetch.util.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+// POST utiliti; return response body and error
+func Post(rpcUrl string, request interface{}) (interface{}, error) {
+	requestBody, marshalErr := json.Marshal(request)
+
+	if marshalErr != nil {
+		log.Printf("[POST] Marshal Request Error: %v", marshalErr)
+		return nil, marshalErr
+	}
+
+	response, fetchErr := http.Post(rpcUrl, "application/json", bytes.NewBuffer(requestBody))
+
+	if fetchErr != nil {
+		log.Printf("[POST] Send Request Error: %v", marshalErr)
+		return nil, fetchErr
+	}
+
+	defer response.Body.Close()
+
+	body, readErr := io.ReadAll(response.Body)
+
+	if readErr != nil {
+		log.Printf("[POST] Read Response Body Error: %v", marshalErr)
+		return nil, readErr
+	}
+
+	return body, nil
+}
