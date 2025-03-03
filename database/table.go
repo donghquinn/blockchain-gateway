ee285a344eb17ae48f3c41db479be819369ab447
donghquinn <ehdgus1524@gmail.com>
2025-03-04 02:29:47 +0900

    Revert "index"
    This reverts commit 1886c8b3b1ef0d0c6a6c8b58f3df3fb51aa28e2e.

--- database/table.go
+++ database/table.go
@@ -10,9 +10,6 @@ var CreateRpcUrlTable = `
 		created_date		DATETIME									NOT NULL DEFAULT 	CURRENT_TIMESTAMP
 	)
 `
-var CreateRpcUrlIndex = `
-	CREATE INDEX rpc_idx ON network_table (network_name, network_type, network_status)
-`
 
 var CreateAccountTable = `
 	CREATE TABLE IF NOT EXISTS account_table (
@@ -26,22 +23,13 @@ var CreateAccountTable = `
 	)
 `
 
-var CreateAccountIndex = `
-	CREATE INDEX account_idx ON account_table (account_status, account_type)
-`
-
 var CreatePrivateKeyTable = `
 	CREATE TABLE IF NOT EXISTS private_key_table (
 		private_key_seq		INTEGER 		PRIMARY KEY AUTOINCREMENT,
 		account_seq			INTEGER 		NOT NULL REFERENCES account_table (account_seq) ON DELETE CASCADE,
 		private_key_dir		TEXT			NOT NULL,
-		private_key_status	INTEGER			NOT NULL DEFAULT 1,
 		created_date 		DATETIME 		NOT NULL DEFAULT 	CURRENT_TIMESTAMP
 	)
-`
-
-var CreatePrivateKeyIndex = `
-	CREATE INDEX private_key_idx ON private_key_idx(private_key_status)
 `
 
 var CreateNonceTable = `
@@ -69,13 +57,9 @@ var CreateTransactionTable = `
 		network_seq			INTEGER													NOT NULL REFERENCES network_table (network_seq),
 		transaction_status  INTEGER													NOT NULL DEFAULT 0,
 		transaction_type	TEXT CHECK(transaction_type IN ('RAW', 'CONTRACT'))		NOT NULL,
-		transaction_hash	TEXT														NULL UNIQUE,
+		transaction_hash	TEXT														NULL,
 		created_date 		DATETIME 												NOT NULL DEFAULT 	CURRENT_TIMESTAMP
 	)
-`
-
-var CreateTransactionIndex = `
-	CREATE INDEX transaction_idx ON transaction_table (transaction_status)
 `
 
 var CreateTransactionDataTable = `
@@ -97,6 +81,4 @@ var CreateTableTransactionQueue = []string{
 	CreateRpcUrlTable,
 	CreateAccountTable, CreatePrivateKeyTable, CreateBalanceTable, CreateNonceTable,
 	CreateTransactionTable, CreateTransactionDataTable,
-
-	CreateAccountIndex, CreatePrivateKeyIndex, CreateRpcUrlIndex, CreateTransactionIndex,
 }
